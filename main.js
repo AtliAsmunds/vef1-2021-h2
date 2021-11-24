2c648951921b67834f0f2daef5ee1e94484f5c3f
Bjarni Þór <bths2@hi.is>
2021-11-24 19:23:30 +0000

    ymislegt

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 import { fetchData } from './lib/data.js';
 import { syncData } from './lib/localstorage.js';
+import { fetchAndRenderPage } from './lib/ui.js';
 
 // Clears storage
 window.localStorage.clear();
 
+// <main> er rót alls efnisins
+const mainEl = document.querySelector('main');
+
 async function main() {
   const json = await fetchData();
   syncData(json);
+  
+  // Render the page into <main>
+  // fetchAndRenderPage(mainEl);
 }
 
 main();
