bf6f249bd995c915304c6f987bf9b29861228048
AtliAsmunds <atlisa20@ru.is>
2021-11-24 01:36:56 +0000

    add async main func

--- main.js
+++ main.js
@@ -4,5 +4,9 @@ import { syncData } from './lib/localstorage.js';
 // Clears storage
 window.localStorage.clear();
 
-const json = await fetchData();
-syncData(json);
+async function main() {
+  const json = await fetchData();
+  syncData(json);
+}
+
+main();
